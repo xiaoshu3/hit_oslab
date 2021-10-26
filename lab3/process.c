689fd1512bfa660b499cf8c9ac639123ac8b6515
xiaoshu3 <longbin314@gmail.com>
2021-10-26 18:23:49 +0800

    add lab3 process.c

---
+++ lab3/process.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/times.h>
+
+#define HZ	100
+
+void cpuio_bound(int last, int cpu_time, int io_time);
+
+int main(int argc, char * argv[])
+{
+	int i,p;
+	for(i=0;i<10;i++){
+		if((p=fork()) == 0){
+			cpuio_bound(20,2*i,20-2*i);
+			exit(0);
+		}
+		else if(p < 0){
+			printf("failed to fork child process %d\n",i+1);
+			return -1;
+		}
+		else{
+			printf("child pid = %d\n",p);
+		}
+	}
+
+	while(1){
+		i = wait(&p);
+		if(i == -1){
+			if(errno == EINTR) continue;
+			//两种情况 没有子进程了或者被其他信号中断
+			break;
+		}
+	}
+
+	return 0;
+}
+
+/*
+ * 此函数按照参数占用CPU和I/O时间
+ * last: 函数实际占用CPU和I/O的总时间，不含在就绪队列中的时间，>=0是必须的
+ * cpu_time: 一次连续占用CPU的时间，>=0是必须的
+ * io_time: 一次I/O消耗的时间，>=0是必须的
+ * 如果last > cpu_time + io_time，则往复多次占用CPU和I/O
+ * 所有时间的单位为秒
+ */
+void cpuio_bound(int last, int cpu_time, int io_time)
+{
+	struct tms start_time, current_time;
+	clock_t utime, stime;
+	int sleep_time;
+
+	while (last > 0)
+	{
+		/* CPU Burst */
+		times(&start_time);
+		/* 其实只有t.tms_utime才是真正的CPU时间。但我们是在模拟一个
+		 * 只在用户状态运行的CPU大户，就像“for(;;);”。所以把t.tms_stime
+		 * 加上很合理。*/
+		do
+		{
+			times(&current_time);
+			utime = current_time.tms_utime - start_time.tms_utime;
+			stime = current_time.tms_stime - start_time.tms_stime;
+		} while ( ( (utime + stime) / HZ )  < cpu_time );
+		last -= cpu_time;
+
+		if (last <= 0 )
+			break;
+
+		/* IO Burst */
+		/* 用sleep(1)模拟1秒钟的I/O操作 */
+		sleep_time=0;
+		while (sleep_time < io_time)
+		{
+			sleep(1);
+			sleep_time++;
+		}
+		last -= sleep_time;
+	}
+}
